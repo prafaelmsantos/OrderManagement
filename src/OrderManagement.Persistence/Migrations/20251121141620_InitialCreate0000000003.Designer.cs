0d1faddc8695e23481d2332789083b0f97cdd2ae
Rafael Santos <96845018+prafaelmsantos@users.noreply.github.com>
2025-11-21 14:16:37 +0000

    3

--- src/OrderManagement.Persistence/Migrations/20251121141620_InitialCreate0000000003.Designer.cs
+++ src/OrderManagement.Persistence/Migrations/20251121141620_InitialCreate0000000003.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OrderManagement.Persistence.Context;
 
@@ -10,9 +11,11 @@ using OrderManagement.Persistence.Context;
 namespace OrderManagement.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251121141620_InitialCreate0000000003")]
+    partial class InitialCreate0000000003
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/OrderManagement.Persistence/Migrations/20251121141620_InitialCreate0000000003.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderManagement.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate0000000003 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "nine_months",
+                table: "product_order",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "nine_months",
+                table: "product_order");
+        }
+    }
+}
